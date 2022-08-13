d0982ec2331ada793e1b70af2b717de8edae8300
Koma Human <hammer.99.ivan@gmail.com>
2022-08-13 18:14:45 +0300

    koma norris

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	images: {
+		domains: ['assets.chucknorris.host'],
+	}
+
+
+
 }
 
 module.exports = nextConfig
